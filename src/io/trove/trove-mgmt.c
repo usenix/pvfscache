f4d40f3785812b9bc9b46679632da03e4600711f
Wei <loonsw@gmail.com>
2012-12-20 00:03:15 -0500

    Revert "Add wormupio"
    This reverts commit 3e5d2d78e4cbdf91ccd6959643bd0678f0741d0d.

--- src/io/trove/trove-mgmt.c
+++ src/io/trove/trove-mgmt.c
@@ -26,8 +26,6 @@ extern struct TROVE_context_ops dbpf_context_ops;
 extern struct TROVE_bstream_ops alt_aio_bstream_ops;
 extern struct TROVE_bstream_ops null_aio_bstream_ops;
 extern struct TROVE_bstream_ops dbpf_bstream_direct_ops;
-extern struct TROVE_bstream_ops dbpf_bstream_wormup_ops;
-
 
 /* currently we only have one method for these tables to refer to */
 struct TROVE_mgmt_ops *mgmt_method_table[] =
@@ -35,8 +33,7 @@ struct TROVE_mgmt_ops *mgmt_method_table[] =
     &dbpf_mgmt_ops,
     &dbpf_mgmt_ops, /* alt-aio */
     &dbpf_mgmt_ops, /* null-aio */
-    &dbpf_mgmt_direct_ops,  /* direct-io */
-    &dbpf_mgmt_ops   /* wormup-io */
+    &dbpf_mgmt_direct_ops  /* direct-io */
 
 };
 
@@ -45,8 +42,7 @@ struct TROVE_dspace_ops *dspace_method_table[] =
     &dbpf_dspace_ops,
     &dbpf_dspace_ops, /* alt-aio */
     &dbpf_dspace_ops, /* null-aio */
-    &dbpf_dspace_ops, /* direct-io */
-    &dbpf_dspace_ops  /* wormup-io */
+    &dbpf_dspace_ops  /* direct-io */
 };
 
 struct TROVE_keyval_ops *keyval_method_table[] =
@@ -54,8 +50,7 @@ struct TROVE_keyval_ops *keyval_method_table[] =
     &dbpf_keyval_ops,
     &dbpf_keyval_ops, /* alt-aio */
     &dbpf_keyval_ops, /* null-aio */
-    &dbpf_keyval_ops,  /* direct-io */
-    &dbpf_keyval_ops   /* wormup-io */
+    &dbpf_keyval_ops  /* direct-io */
 };
 
 struct TROVE_bstream_ops *bstream_method_table[] =
@@ -63,8 +58,7 @@ struct TROVE_bstream_ops *bstream_method_table[] =
     &dbpf_bstream_ops,
     &alt_aio_bstream_ops,
     &null_aio_bstream_ops,
-    &dbpf_bstream_direct_ops,
-    &dbpf_bstream_wormup_ops
+    &dbpf_bstream_direct_ops
 };
 
 struct TROVE_context_ops *context_method_table[] =
@@ -72,8 +66,7 @@ struct TROVE_context_ops *context_method_table[] =
     &dbpf_context_ops,
     &dbpf_context_ops, /* alt-aio */
     &dbpf_context_ops, /* null-aio */
-    &dbpf_context_ops, /* direct-io */
-    &dbpf_context_ops, /* wormup-io */
+    &dbpf_context_ops  /* direct-io */
 };
 
 /* trove_init_mutex, trove_init_status
